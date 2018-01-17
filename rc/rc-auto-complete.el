fb78bc49533525c91c24cd13ddf437601b716fc6
Shaka Chen <scchen@libgirl.com>
2018-01-17 10:03:34 +0800

    added ac-source-words-in-all-buffer

--- rc/rc-auto-complete.el
+++ rc/rc-auto-complete.el
@@ -1,3 +1,5 @@
 (ac-config-default)
 (global-auto-complete-mode t)
+(setq ac-sources
+      '(ac-source-words-in-all-buffer ac-source-features ac-source-functions ac-source-yasnippet ac-source-variables ac-source-symbols ac-source-abbrev ac-source-dictionary))
 (setq ac-auto-start 4)
