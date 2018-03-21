f4c86959b8f3a6b78b29167540db5dbaa55121bd
Shaka Chen <scchen@libgirl.com>
2018-03-21 17:27:59 +0800

    updated for updated roswell. And made more minimal

--- init.el
+++ init.el
@@ -8,16 +8,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(font-lock-global-modes (quote (not speedbar-mode)))
- '(js-indent-level 4)
+ '(indent-tabs-mode nil)
+ '(js-indent-level 2)
+ '(js-switch-indent-offset 2)
  '(package-selected-packages
    (quote
-    (ac-slime auto-complete helm-descbinds blackboard-theme cherry-blossom-theme silkworm-theme popup slime magit js2-mode monokai-theme zenburn-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+    (ac-slime auto-complete helm-descbinds blackboard-theme popup magit js2-mode))))
 
 (mapcar (lambda (init-filename)
 	  (load (expand-file-name (format "~/.emacs.d/rc/%s.el"
@@ -26,9 +22,14 @@
 	  "rc-tramp"
 	  "rc-auto-complete"
 	  "rc-js"
-	  "rc-common-lisp"
+          "rc-common-lisp"
 	  "rc-general-customization"))
+
 (load (expand-file-name "~/.emacs.d/scriba/scriba.el"))
 
-(slime)
-
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- rc/rc-common-lisp.el
+++ rc/rc-common-lisp.el
@@ -1,5 +1,3 @@
-(add-to-list 'auto-mode-alist '("\\.clp\\'" . html-mode))
-
 ;; setup ac-slime
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
@@ -9,16 +7,9 @@
 ;;setup roswell
 ;; by defvar, this setting won't be overwritten in ~/.roswell/helper.el
 (defvar roswell-slime-contribs '(slime-fancy
-				 slime-indentation
-				 slime-sbcl-exts
-				 slime-repl-ansi-color ; remember at least to create a symbolic link to slime/contrib 
-				 slime-tramp))
+				 slime-repl-ansi-color)) ; remember at least to create a symbolic link to slime/contrib 
 
 (setq slime-contribs roswell-slime-contribs)
-(load (expand-file-name "~/.emacs.d/rc/rc-roswell.el"))
-(require 'slime-autoloads)
-(slime-setup slime-contribs)
-(setq inferior-lisp-program "ros -Q run")
-;;(setq inferior-lisp-program "/usr/local/bin/sbcl") ; for using native sbcl
+(load (expand-file-name "~/.roswell/helper.el"))
 
 
