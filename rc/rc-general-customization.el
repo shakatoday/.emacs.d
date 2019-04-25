9e701120c96f72b893639a121ad46f607b0bed72
Shaka Chen <scchen@libgirl.com>
2019-04-25 12:09:27 +0800

    set backup directory

--- rc/rc-general-customization.el
+++ rc/rc-general-customization.el
@@ -2,3 +2,9 @@
 (put 'downcase-region 'disabled nil)
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying      t  ; Don't de-link hard links
+      version-control        t  ; Use version numbers on backups
+      delete-old-versions    t  ; Automatically delete excess backups:
+      kept-new-versions      6 ; how many of the newest versions to keep
+      kept-old-versions      2) ; and how many of the old
