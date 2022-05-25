831617a048262ef01c88ef422eb9110241e5dafc
Shaka Chen <scchen@shaka.today>
2022-05-25 16:43:42 +0800

    no autosave no backup

--- rc/rc-general-customization.el
+++ rc/rc-general-customization.el
@@ -3,10 +3,6 @@
 (put 'downcase-region 'disabled nil)
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-      backup-by-copying      t  ; Don't de-link hard links
-      version-control        t  ; Use version numbers on backups
-      delete-old-versions    t  ; Automatically delete excess backups:
-      kept-new-versions      6 ; how many of the newest versions to keep
-      kept-old-versions      2) ; and how many of the old
+(setq make-backup-files nil)
+(setq auto-save-default nil)
 (setq column-number-mode t)
