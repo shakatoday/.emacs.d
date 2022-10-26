a8d029b06f426d8c841488258e83b488853711a0
Shaka Chen <scchen@shaka.today>
2022-10-26 20:12:33 +0800

    rc/rc-web.el already included the commented setting

--- rc/rc-tsx.el
+++ rc/rc-tsx.el
@@ -1,9 +1,9 @@
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (setup-tide-mode))))
-;; enable typescript-tslint checker
-(with-eval-after-load 'flycheck
-  (flycheck-add-mode 'typescript-tslint 'web-mode))
+;; (add-hook 'web-mode-hook
+;;           (lambda ()
+;;             (when (string-equal "tsx" (file-name-extension buffer-file-name))
+;;               (setup-tide-mode))))
+;; ;; enable typescript-tslint checker
+;; (with-eval-after-load 'flycheck
+;;   (flycheck-add-mode 'typescript-tslint 'web-mode))
