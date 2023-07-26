59adb2e1cc1933544e72af0da18e12459ee626d2
Shaka Chen <scchen@shaka.today>
2023-07-26 15:38:31 +0800

    switched from auto-complete mode to company-mode

--- rc/rc-company.el
+++ rc/rc-company.el
@@ -2,4 +2,5 @@
 
 (setq company-idle-delay 0.1)
 (setq company-dabbrev-downcase nil)
+(setq company-backends '((company-capf :with company-yasnippet :with company-dabbrev-code)))
 (setq company-tooltip-align-annotations t)
