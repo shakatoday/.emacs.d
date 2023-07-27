f179f372c62912344f23a1860b5d1f2051ef4e55
Shaka Chen <scchen@shaka.today>
2023-07-27 13:10:26 +0800

    inhibit "Warning: default bindings are being changed to C-n and C-p"

--- rc/rc-company.el
+++ rc/rc-company.el
@@ -1,5 +1,11 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
+;; Supress "Warning: default bindings are being changed to C-n and C-p"
+(defadvice message (around my-message-filter activate)
+  (unless (string-prefix-p "Warning: default bindings are being changed to C-n and C-p"
+                           (or (ad-get-arg 0) ""))
+    ad-do-it))
+
 (setq company-idle-delay 0.1)
 (setq company-dabbrev-downcase nil)
 (setq company-backends '(company-tabnine
