9e8c2d11ea51e05e002c4d7771ecc147404105e3
Shaka Chen <scchen@shaka.today>
2023-12-26 16:27:47 +0800

    eglot-ensure for python-mode

--- init.el
+++ init.el
@@ -93,7 +93,7 @@
 
 (use-package eglot
   :ensure t
-  :hook ((tsx-ts-mode typescript-ts-mode html-mode css-ts-mode scss-mode json-ts-mode rust-mode) . eglot-ensure)
+  :hook ((tsx-ts-mode typescript-ts-mode html-mode css-ts-mode scss-mode json-ts-mode rust-mode python-mode) . eglot-ensure)
   :config
   ;;
   ;; Performance tweak. See https://www.gnu.org/software//emacs/manual/html_node/eglot/Performance.html
